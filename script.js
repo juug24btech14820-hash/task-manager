b8b81eddcc64138fe12d19c77c9b607b0ae0535d
juug24btech14820-hash <juug24btech14820@jainuniversity.ac.in>
2026-04-23 11:05:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-// Load tasks when page opens
-window.onload = function () {
-    loadTasks();
-};
-
 function addTask() {
     let input = document.getElementById("taskInput");
     let task = input.value;
 
+    alert("Button clicked"); // TEST LINE
+
     if (task === "") {
-        alert("Enter a task!");
+        alert("Enter something!");
         return;
     }
 
     let li = document.createElement("li");
-
-    li.innerHTML = task +
-        " <button onclick='completeTask(this)'>✔</button>" +
-        " <button onclick='deleteTask(this)'>❌</button>";
+    li.innerHTML = task;
 
     document.getElementById("taskList").appendChild(li);
 
-    saveTasks();
     input.value = "";
 }
-
-function completeTask(btn) {
-    btn.parentElement.classList.toggle("completed");
-    saveTasks();
-}
-
-function deleteTask(btn) {
-    btn.parentElement.remove();
-    saveTasks();
-}
-
-// Save tasks to browser
-function saveTasks() {
-    let tasks = document.getElementById("taskList").innerHTML;
-    localStorage.setItem("tasks", tasks);
-}
-
-// Load tasks from browser
-function loadTasks() {
-    let saved = localStorage.getItem("tasks");
-    if (saved) {
-        document.getElementById("taskList").innerHTML = saved;
-    }
-}
-
-// Clear all tasks
 function clearAll() {
     document.getElementById("taskList").innerHTML = "";
     localStorage.removeItem("tasks");
